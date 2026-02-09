b940d007a9f24f985c4407b1b9f2324f7b267db9
DanGLVK <switchtegra@gmail.com>
2026-02-09 04:47:25 -0300

    Update verify.sh

--- magisk-loader/magisk_module/verify.sh
+++ magisk-loader/magisk_module/verify.sh
@@ -64,7 +64,7 @@ hash_path="$file_path.sha256"
 unzip -o "$ZIPFILE" "META-INF/com/google/android/*" -d "$TMPDIR_FOR_VERIFY" >&2
 [ -f "$file_path" ] || abort_verify "$file not exists"
 if [ -f "$hash_path" ]; then
-  (echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -) || abort_verify "Failed to verify $file"
+  (echo "$(cat "$hash_path")  $file_path" | sha256sum -c -s -)
   ui_print "- Verified $file" >&1
 else
   ui_print "- Download from Magisk app"
